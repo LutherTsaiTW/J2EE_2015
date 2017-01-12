6c122cb64967feba7aac02d5872c413e80fe6f76
LutherTsaiTW <Luther0429@gmail.com>
2017-01-12 10:12:44 +0800

    Fix uncommon changes

---
+++ TestMVN_Spring/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package main.java.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+		System.out.println("The current local time is: ");
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
+}
